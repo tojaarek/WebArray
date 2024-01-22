294f71c5058f968993713a83319fd6ee4b9592cd
tojaarek <119487863+tojaarek@users.noreply.github.com>
2024-01-22 19:10:22 +0100

    slider

--- src/js/header.js
+++ src/js/header.js
@@ -1,8 +1,4 @@
 const header = document.querySelector('header');
-const offices = document.querySelector('#offices');
-const customers = document.querySelector('#customers');
-const workers = document.querySelector('#workers');
-const projects = document.querySelector('#projects');
 
 window.addEventListener('scroll', () => {
   const position = window.scrollY;
