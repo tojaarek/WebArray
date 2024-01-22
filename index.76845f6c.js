e460eca616d9a4891e3972e718c92db9169d34e3
tojaarek <arek@wilczynski.app>
2024-01-22 18:11:12 +0000

    Deploying to gh-pages from @ tojaarek/WebArray@294f71c5058f968993713a83319fd6ee4b9592cd 🚀

--- index.76845f6c.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("header");document.querySelector("#offices"),document.querySelector("#customers"),document.querySelector("#workers"),document.querySelector("#projects");window.addEventListener("scroll",(()=>{const o=window.scrollY;e.style.backgroundColor=o>30?"white":"transparent"}));
-//# sourceMappingURL=index.76845f6c.js.map
---
+++ index.cd98d77f.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("header");window.addEventListener("scroll",(()=>{const o=window.scrollY;e.style.backgroundColor=o>30?"white":"transparent"}));
+//# sourceMappingURL=index.cd98d77f.js.map
